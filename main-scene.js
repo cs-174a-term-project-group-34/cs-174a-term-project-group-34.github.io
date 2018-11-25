a8c1f4e73134876fb3144587eed010602c605aaf
prathyushk <prathyush.katukojwala@gmail.com>
2018-11-24 23:54:56 -0800

    Make the world a bigger place

--- main-scene.js
+++ main-scene.js
@@ -9,9 +9,9 @@ class Final_Project extends Scene_Component
         this.initial_camera_location = Mat4.inverse( context.globals.graphics_state.camera_transform );
 
         const r = context.width/context.height;
-        context.globals.graphics_state.projection_transform = Mat4.perspective( Math.PI/4, r, .1, 1000 );
+        context.globals.graphics_state.projection_transform = Mat4.perspective( Math.PI/4, r, .1, 1500 );
 
-	create_height_map("assets/heightmap.jpg", 128, -50, 50, (gp) => {
+	create_height_map("assets/heightmap.jpg", 128, -200, 300, (gp) => {
             const shapes = { skybox_side: new Square(),
 			     map: gp
 			   };
@@ -29,7 +29,7 @@ class Final_Project extends Scene_Component
 		context.get_instance( Phong_Shader ).material( Color.of( 0, 0, 0, 1 ), { ambient: 1, specularity: 0, diffusivity: 0, texture: context.get_instance( "assets/skybox/bk.png", true ) }),
 		context.get_instance( Phong_Shader ).material( Color.of( 0, 0, 0, 1 ), { ambient: 1, specularity: 0, diffusivity: 0, texture: context.get_instance( "assets/skybox/ft.png", true ) })
 	    ],
-	    map: context.get_instance( Phong_Shader ).material(Color.of( 0, 0, 0, 1 ), { ambient: 1, specularity: 0, diffusivity: 0, texture: context.get_instance( "assets/terrain.jpg", true) } )
+	    map: context.get_instance( Phong_Shader ).material(Color.of( 0, 0, 0, 1 ), { ambient: 0.5, specularity: 0, diffusivity: 0.8, texture: context.get_instance( "assets/terrain.jpg", true) } )
 	}
 
         this.lights = [ new Light( Vec.of( -5,5,5,1 ), Color.of( 0,1,1,1 ), 100000 ) ];
@@ -43,15 +43,15 @@ class Final_Project extends Scene_Component
         const t = graphics_state.animation_time / 1000, dt = graphics_state.animation_delta_time / 1000;
 
 	this.draw_skybox(graphics_state);
-	this.shapes.map.draw(graphics_state, Mat4.scale([200,1,200]).times(Mat4.translation([-0.5,0,-0.5])), this.materials.map);
+	this.shapes.map.draw(graphics_state, Mat4.scale([1000,1,1000]).times(Mat4.translation([-0.5,0,-0.5])), this.materials.map);
     }
     draw_skybox(graphics_state) {
 	for( var i = 0; i < 3; i++ )                    
             for( var j = 0; j < 2; j++ )
         { var square_transform = Mat4.rotation( i == 0 ? Math.PI/2 : 0, Vec.of(1, 0, 0) )
               .times( Mat4.rotation( Math.PI * j - ( i == 1 ? Math.PI/2 : 0 ), Vec.of( 0, 1, 0 ) ) )
-              .times( Mat4.translation([ 0, 0, 100 ]) )
-	      .times( Mat4.scale([100.5,100.5,100.5]) );
+              .times( Mat4.translation([ 0, 0, 500 ]) )
+	      .times( Mat4.scale([501.5,501.5,501.5]) );
 	  this.shapes.skybox_side.draw( graphics_state, square_transform, this.materials.skybox[2*i + j] );
         }
     }
